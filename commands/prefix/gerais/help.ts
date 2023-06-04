4cb661ded459dc51943a4cb093350f6be7720711
Marciel404 <103785110+Marciel404@users.noreply.github.com>
2023-06-04 15:40:54 -0300

    Mudando para TypeScript

--- commands/prefix/gerais/help.ts
+++ commands/prefix/gerais/help.ts
@@ -1,8 +1,10 @@
+import { Message } from "discord.js";
+
 module.exports = {
     name: "help",
     aliases: ["ajuda"],
     description: "Envia meus comandos",
-    async execute(msg) {
-
+    async execute(msg: Message) {
+        
     }
 }
