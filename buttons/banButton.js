8f1d696c268ae4d4da0f9d721a7cf023b0b8c812
Marciel404 <103785110+Marciel404@users.noreply.github.com>
2023-06-03 22:19:16 -0300

    Add files via upload

--- buttons/banButton.js
+++ buttons/banButton.js
@@ -1,15 +1,178 @@
+const { MessageCollector, ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } = require("discord.js")
 const { verifyRoles } = require("../funcsSuporte/verifyRoles")
+const { EmbedBuilder } = require("@discordjs/builders")
+const client = require("../utils/loader")
 const configData = require(`../utils/config${process.env.bot}`)
+
+const motivos = [
+    {
+        "emoji": ":BAN:903237786465894430",
+        "label": 'Flood/spam.',
+        "value": '1',
+    },
+    {
+        "emoji": ":BAN:903237786465894430",
+        "label": 'Divulgação inadequada.',
+        "value": '2',
+    },
+    {
+        "emoji": ":BAN:903237786465894430",
+        "label": 'Off topic/mensagem fora de tópico.',
+        "value": '3',
+    },
+    {
+        "emoji": ":BAN:903237786465894430",
+        "label": 'Menção desnecessária de membros e cargos.',
+        "value": '4',
+    },
+    {
+        "emoji": ":BAN:903237786465894430",
+        "label": 'Provocação e brigas.',
+        "value": '5',
+    },
+    {
+        "emoji": ":BAN:903237786465894430",
+        "label": 'Poluição sonora.',
+        "value": '6',
+    },
+    {
+        "emoji": ":BAN:903237786465894430",
+        "label": 'Atrapalhar o andamento do Karaokê.',
+        "value": '7',
+    },
+    {
+        "emoji": ":BAN:903237786465894430",
+        "label": 'Denúncias falsas.',
+        "value": '8',
+    },
+    {
+        "emoji": ":BAN:903237786465894430",
+        "label": 'Linguagem discriminatória.',
+        "value": '9',
+    },
+    {
+        "emoji": ":BAN:903237786465894430",
+        "label": 'Exposição de membros/ Assédio.',
+        "value": '10',
+    },
+    {
+        "emoji": ":BAN:903237786465894430",
+        "label": 'Preconceito, discriminação, difamação e/ou desrespeito.',
+        "value": '11',
+    },
+    {
+        "emoji": ":BAN:903237786465894430",
+        "label": 'Planejar ou exercer raids no servidor.',
+        "value": '12',
+    },
+    {
+        "emoji": ":BAN:903237786465894430",
+        "label": 'NSFW/ (+18).',
+        "value": '13',
+    },
+    {
+        "emoji": ":BAN:903237786465894430",
+        "label": 'Estimular ou praticar atividades ilegais ou que cause banimento de membros.',
+        "value": '14',
+    },
+    {
+        "emoji": ":BAN:903237786465894430",
+        "label": 'Evasão de punição.',
+        "value": '15',
+    },
+    {
+        "emoji": ":BAN:903237786465894430",
+        "label": 'Conteúdos graficamente chocantes.',
+        "value": '16',
+    },
+    {
+        "emoji": ":BAN:903237786465894430",
+        "label": 'Quebra do ToS do Discord.',
+        "value": '17',
+    },
+    {
+        "emoji": ":BAN:903237786465894430",
+        "label": 'Selfbot.',
+        "value": '18',
+    },
+    {
+        "emoji": ":BAN:903237786465894430",
+        "label": 'Scam.',
+        "value": '19',
+    },
+
+]
 
 module.exports = {
     roles: [
         configData["roles"]["staff"]["staff1"],
         configData["roles"]["staff"]["staff2"]
     ],
-    async execute( msg ) {
+    async execute( interaction ) {
 
-        if (!verifyRoles(msg, this.roles)) return
-        await msg.reply({content: `123`, ephemeral: true})
+        if (!verifyRoles(interaction, this.roles)) return await interaction.reply({content: "Sem permissão", ephemeral: true})
+
+        const selecMenu = new StringSelectMenuBuilder()
+        .setCustomId("motivos")
+        .setMinValues(1)
+        .setPlaceholder("Motivo do banimento")
         
+        for (const i of motivos) {
+            selecMenu.addOptions(
+                new StringSelectMenuOptionBuilder()
+                .setLabel(i["label"])
+                .setEmoji(i["emoji"])
+                .setValue(i["value"])
+            )
+        }
+
+        const row = new ActionRowBuilder()
+        .addComponents(selecMenu)
+
+        await interaction.reply({content: "Envie os ids", ephemeral: true})
+        const collectorFilter = (user) => {
+            return user.id === interaction.user.id;
+        };
+        const collector = new MessageCollector(interaction.channel,collectorFilter, { time: 10000 });
+        collector.on("collect", async (message) => {
+
+            if (message.author.id === interaction.user.id){
+
+                collector.stop()
+
+                let desc = ""
+                const embed = new EmbedBuilder()
+                .setTitle("Banimento")
+                .setFooter({text: interaction.user.id})
+
+                const client = await interaction.guild.members.fetch(interaction.client.user.id)
+
+                for (const i of message.content.split("\n")){
+
+                    let member = await interaction.guild.members.fetch(i.replace(/[<@>]/g, ""))
+
+                    if (member.roles.highest.position >= client.roles.highest.position){
+                        let msg = await interaction.channel.send({content: `Não consego banir o membro ${member}`});
+                        setTimeout(async () =>{
+                            await msg.delete()
+                        }, 5000)
+                    } else {
+                        desc += `${member}\n`
+                    }
+                }
+                embed.setDescription(desc)
+
+                await interaction.editReply(
+                    {
+                        content: "Qual motivo?",
+                        embeds: [embed],
+                        components:[row]
+                    }
+                )
+
+                await message.delete()
+                
+            }
+        })
     }
 }
--- buttons/confirmBan.js
+++ buttons/confirmBan.js
@@ -1,15 +1,68 @@
+const { EmbedBuilder } = require("@discordjs/builders")
+const { verifyRoles } = require("../funcsSuporte/verifyRoles")
+const configData = require(`../utils/config${process.env.bot}`)
+
 module.exports = {
     roles: [
         configData["roles"]["staff"]["asmodeus"],
         configData["roles"]["staff"]["astaroth"]
     ],
-    async execute( msg ) {
+    async execute( interaction ) {
 
-        if (!verifyRoles(msg, this.roles)) return 
+        let eR = new EmbedBuilder()
+        .setTitle("Banimento")
 
-        
-        await msg.guild.members.ban(msg.content.toLowerCase().split(" ")[1].replace(/[<@>]/gi), {reason: `${reason}`})
-        await msg.reply({content: `${reason}`})
+        if (!verifyRoles(interaction, this.roles)) return interaction.reply({content: "Sem permissão"})
+
+        for (const m of interaction.message.embeds[0].description.split("\n")){
+
+            let user = await interaction.client.users.fetch(m.replace(/[<@>]/g, ""))
+            const author = await interaction.guild.members.fetch(interaction.message.embeds[0].footer.text)
+            const aprovador = interaction.member
+            const reason = interaction.message.embeds[0].fields[0].value
+
+            try {
+
+                try {
+                    eR.setFields(
+                        {name: "Banido por", value: `${author.user.username}`,inline: false},
+                        {name: "Aprovador por", value: `${aprovador.user.username}`, inline: false},
+                        {name: "Motivo", value: `${reason}`, inline: false}
+                    )
+                    await user.send({embeds: [eR]})
+                } catch (err) {
+                    console.log(err)
+                }
+
+                eR.setFields(
+                    {name: "Membro Banido", value: `${user.username}`, inline: false},
+                    {name: "Banido por", value: `${author}`,inline: false},
+                    {name: "Aprovador por", value: `${aprovador}`, inline: false},
+                    {name: "Motivo", value: `${reason}`, inline: false}
+                )
+                await interaction.guild.bans.create(
+                    m.replace(/[<@>]/g, ""),
+                    {
+                        reason: reason,
+                        deleteMessageSeconds: 604800
+                    });
+                interaction.guild.channels.fetch(configData["channels"]["modlog"])
+                .then( async (channel) => {
+                    await channel.send({
+                        embeds: [eR],
+                    });
+                })
+                .catch ( async (err) => {
+                });
+            } catch (err) {
+                let msg = await interaction.channel.send({content: `Não conseguir banir o membro <@${m.replace(/[<@>]/g, "")}>`});
+                setTimeout(async ()=>{
+                    await msg.delete()
+                }, 5000);
+            };
+        }
+
+        await interaction.message.delete()
         
     }
 }
