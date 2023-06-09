ee35031be4b3aee703ff1773f38500daefe3a9f9
Marciel404 <103785110+Marciel404@users.noreply.github.com>
2023-06-09 17:22:55 -0300

    Delete convert directory

--- convert/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv").config();
-const { range } = require("discord.js");
-const loader_1 = require("./utils/loader");
-loader_1.client.login(process.env.token);
