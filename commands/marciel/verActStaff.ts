1cb4dec474502fc89bbf7165db0fa2cc529b8acd
Marciel404 <103785110+Marciel404@users.noreply.github.com>
2023-08-08 18:25:15 -0300

    Add files via upload

---
+++ commands/marciel/verActStaff.ts
@@ -0,0 +1,45 @@
+import { ChatInputCommandInteraction, InteractionType, Message, SlashCommandBuilder } from "discord.js"
+import { configData } from "../..";
+import { verifyRolesPermissions, verifyUserId } from "../../funcsSuporte/verifys";
+import actStaff from "../../utils/dbActStaff";
+
+const roles: Array<string> = [
+    configData["roles"]["staff"]["asmodeus"],
+    configData["roles"]["staff"]["astaroth"],
+    configData["roles"]["staff"]["ormenus"],
+    configData["roles"]["staff"]["acacus"]
+]
+
+export = {
+    data: new SlashCommandBuilder()
+        .setName("vas")
+        .setDescription("????")
+        .setDMPermission(false),
+    name: "veractivitystaff",
+    aliases: ["vacts", "vas"],
+    roles: [
+        configData["roles"]["staff"]["asmodeus"],
+    ],
+    description: "Mostra a ultima aparição dos membros da staff constada pelo bot",
+    async execute(msg: Message | ChatInputCommandInteraction) {
+
+        if (!msg.guild) return
+
+        if (verifyRolesPermissions(msg.member!, this.roles) || verifyUserId(msg.member!.user, ["485801281621852175"])) {
+            if (msg.type == InteractionType.ApplicationCommand) {
+                msg.deferReply({ ephemeral: true })
+            }
+
+            const membersStaff = (await msg.guild?.members.fetch()).filter(m => verifyRolesPermissions(m, roles))
+
+            const e = await actStaff.embedData(membersStaff)
+
+            if (msg.type == InteractionType.ApplicationCommand) {
+                msg.followUp({ embeds: [e], ephemeral: true })
+            } else {
+                msg.reply({ embeds: [e] })
+            }
+        }
+
+    }
+}
