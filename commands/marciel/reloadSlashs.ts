fb58a8485783a268a20209fbfe963d82aba20ec3
Marciel404 <103785110+Marciel404@users.noreply.github.com>
2023-08-09 20:30:00 -0300

    Correções bobas de import e typagem

--- commands/marciel/reloadSlashs.ts
+++ commands/marciel/reloadSlashs.ts
@@ -1,12 +1,12 @@
-import { ChatInputCommandInteraction, Message } from "discord.js";
-import { loadCommandsSlash, loadSlash } from "../../utils/loaders";
+import { Message } from "discord.js";
+import { loadCommandsSlash, loadSlash } from "../../utils/Loaders";
 import { verifyUserId } from "../../funcsSuporte/verifys"
 
 export = {
     name: "reloadSlashs",
     description: "Reinicia os slashs do bot",
     aliases: ["rslash"],
-    async execute(msg: Message | ChatInputCommandInteraction){
+    async execute(msg: Message){
 
         if (!verifyUserId(msg.author,["485801281621852175"])) return
 
